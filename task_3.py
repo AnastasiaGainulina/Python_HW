241ed585a4c199abdb5caf96af2e99b3d58b5895
AnastasiaGainulina <109802756+AnastasiaGainulina@users.noreply.github.com>
2023-07-11 19:15:52 +0300

    Create task_3.py

---
+++ task_3.py
@@ -0,0 +1,12 @@
+a = int(input('Напишите число: '))
+
+def fib_gen():
+    a, b = 0, 1
+    while 1:
+        yield a
+        a, b = b, a + b
+
+fs = fib_gen()
+next(fs)
+for i in range(a):
+    print(next(fs))
